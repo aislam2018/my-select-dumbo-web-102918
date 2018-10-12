7d34ac7ab8f08160f182db42b02f719fd884798e
anik1606 <anik1606@gmail.com>
2018-10-12 17:39:44 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,9 +2,9 @@ def my_select(collection)
  new_arr = []
  i = 0 
  while i < collection 
- if yiled(arr[i]) == true
-   new_arr << arr[i]
- end
+  if yield(arr[i]) == true 
+    new_arr << arr[i]
+
  i += 1 
  end
  new_arr
